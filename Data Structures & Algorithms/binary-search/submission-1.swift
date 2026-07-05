44131106af131879e7af40a150a7822896c71f85
Hirendra Sharma <hks.it11@gmail.com>
2026-07-05 17:03:51 +0530

    Add: binary-search - submission-1

---
+++ Data Structures & Algorithms/binary-search/submission-1.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left = 0
+        var right = nums.count - 1
+        while left <= right {
+            let mid = (left + right)/2
+            if nums[mid] == target { return mid }
+            if nums[left] == target { return left }
+            if nums[right] == target { return right }
+
+            if target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        return -1
+    }
+}

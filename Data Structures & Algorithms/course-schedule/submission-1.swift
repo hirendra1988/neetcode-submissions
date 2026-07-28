7b98e51ed5bac32d64d67462e6adbe70d3472696
Hirendra Sharma <hks.it11@gmail.com>
2026-07-28 21:12:25 +0530

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.swift
@@ -0,0 +1,40 @@
+class Solution {
+    func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
+        let n = numCourses
+        var graph = [[Int]](repeating: [], count: n)
+        for prerequisite in prerequisites {
+            let u = prerequisite[0]
+            let v = prerequisite[1]
+            graph[v].append(u)
+        }
+
+        var inDegree = [Int](repeating: 0, count: n)
+        for adj in graph {
+            for i in adj {
+                inDegree[i] += 1
+            }
+        }
+
+        var queue = [Int]()
+        for i in 0..<n {
+            if inDegree[i] == 0 {
+                queue.append(i)
+            }
+        }
+
+        var completedCourse = 0
+
+        while !queue.isEmpty {
+            completedCourse += 1
+            var currentCourse = queue.removeFirst()
+            for next in graph[currentCourse] {
+                inDegree[next] -= 1
+                if inDegree[next] == 0 {
+                    queue.append(next)
+                }
+            }
+        }
+
+        return completedCourse == n
+    }
+}

c45ce239af3e8f3c055076cc654a1b351a30a964
Hirendra Sharma <hks.it11@gmail.com>
2026-07-29 11:25:03 +0530

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func rob(_ nums: [Int]) -> Int {
+        if nums.isEmpty {
+            return 0
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+        return max(robHelper(Array(nums.dropFirst())), robHelper(Array(nums.dropLast())))
+    }
+
+    func robHelper(_ nums: [Int]) -> Int {
+        if nums.isEmpty {
+            return 0
+        }
+        if nums.count == 1 {
+            return nums[0]
+        }
+        let n = nums.count
+        var dp = [Int](repeating: 0, count: n)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in 2..<n {
+            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+        }
+        return dp[n-1]
+    }
+}

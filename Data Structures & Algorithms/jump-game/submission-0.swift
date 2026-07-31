b251d5fdd84f2fd5dd008bc6dd656ddd2106592f
Hirendra Sharma <hks.it11@gmail.com>
2026-07-31 19:00:43 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var target = nums.count - 1
+        for i in stride(from: nums.count-1, through: 0, by: -1) {
+            if (nums[i] + i) >= target {
+                target = i
+            }
+        }
+        return target == 0
+    }
+}

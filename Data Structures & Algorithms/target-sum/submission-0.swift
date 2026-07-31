07b492052f0411bed3f132f09a01267ce4529517
Hirendra Sharma <hks.it11@gmail.com>
2026-07-31 14:50:02 +0530

    Add: target-sum - submission-0

---
+++ Data Structures & Algorithms/target-sum/submission-0.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func findTargetSumWays(_ nums: [Int], _ target: Int) -> Int {
+        var dp = [String: Int]()
+        return helper(nums, target, 0, &dp)
+    }
+
+    func helper(_ nums: [Int], _ target: Int, _ index: Int, _ dp: inout [String: Int]) -> Int {
+        if index >= nums.count {
+            return target == 0 ? 1 : 0
+        }
+        if let cache = dp["\(target),\(index)"] {
+            return cache
+        }
+        let plus = helper(nums, target + nums[index], index+1, &dp)
+        let minus = helper(nums, target - nums[index], index+1, &dp)
+        dp["\(target),\(index)"] = plus + minus
+        return dp["\(target),\(index)"]!
+    }
+}

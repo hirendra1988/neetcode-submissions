e423eb13d47a3a4bdafb081ec903252e516207ad
Hirendra Sharma <hks.it11@gmail.com>
2026-08-08 14:04:01 +0530

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func isHappy(_ n: Int) -> Bool {
+        if n == 1 || n == 7 || n == 10 {
+            return true
+        }
+        if n < 10 {
+            return false
+        }
+        var n = n
+        var carry = 0
+        var multi = 0
+        while n > 0 {
+            let val = n % 10
+            multi += val * val
+            n = n / 10
+        }
+        return isHappy(multi)
+    }
+}

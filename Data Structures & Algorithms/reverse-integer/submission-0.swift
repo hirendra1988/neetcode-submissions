b5fbe6cd073deb052eca8649980330f96514accd
Hirendra Sharma <hks.it11@gmail.com>
2026-08-13 10:32:37 +0530

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        var n = x
+        var result = 0
+        var minVal = Int32.min
+        var maxVal = Int32.max
+        var carry = 0
+        while n != 0 {
+            carry = n % 10
+            if (result > maxVal/10) || ((result == maxVal/10) && (carry > maxVal % 10)) {
+                return 0
+            }
+            if (result < minVal/10) || ((result == minVal/10) && (carry < minVal % 10)) {
+                return 0
+            }
+            result = result * 10 + carry
+            n = n / 10
+        }
+        return result
+    }
+}

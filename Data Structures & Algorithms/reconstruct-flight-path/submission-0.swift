48f414345961785ca2853d64a27732f96ffe0e0f
Hirendra Sharma <hks.it11@gmail.com>
2026-08-18 10:15:07 +0530

    Add: reconstruct-flight-path - submission-0

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-0.swift
@@ -0,0 +1,28 @@
+class Solution {
+    func findItinerary(_ tickets: [[String]]) -> [String] {
+        if tickets.isEmpty {
+            return []
+        }
+        var sTickets = tickets.sorted { $0[1] < $1[1] }
+        var adj = [String: [String]]()
+        for ticket in sTickets {
+            let from = ticket[0]
+            let to = ticket[1]
+            adj[from, default: []].append(to)
+        } 
+        var result = [String]()
+        dfs(&adj, &result, "JFK")
+        return result.reversed()
+    }
+
+    func dfs(_ adj: inout [String: [String]],
+             _ result: inout [String],
+             _ ticket: String) {
+        if adj[ticket] != nil {
+            while !adj[ticket]!.isEmpty {
+                dfs(&adj, &result, adj[ticket]!.removeFirst())
+            }
+        }
+        result.append(ticket)
+    }
+}

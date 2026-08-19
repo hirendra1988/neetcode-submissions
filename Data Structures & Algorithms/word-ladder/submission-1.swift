216ed52c21cd3f8df97f574edf748dc3abd23132
Hirendra Sharma <hks.it11@gmail.com>
2026-08-19 10:28:44 +0530

    Add: word-ladder - submission-1

---
+++ Data Structures & Algorithms/word-ladder/submission-1.swift
@@ -0,0 +1,35 @@
+class Solution {
+    func ladderLength(_ beginWord: String, _ endWord: String, _ wordList: [String]) -> Int {
+        let alphabets = "abcdefghijklmnopqrstuvwxyz"
+        var setWords = Set(wordList)
+        if !setWords.contains(endWord) {
+            return 0
+        }
+        var queue = [(String, Int)]()
+        queue.append((beginWord, 1))
+
+        var index = 0
+
+        while index < queue.count {
+            let (word, step) = queue[index]
+            index += 1
+            if word == endWord {
+                return step
+            }
+            var wordArray = Array(word)
+            for i in 0..<wordArray.count {
+                let temp = wordArray[i]
+                for ch in alphabets where ch != wordArray[i] {
+                    wordArray[i] = ch
+                    let modifiedStr = String(wordArray)
+                    if setWords.contains(modifiedStr) {
+                        queue.append((modifiedStr, step + 1))
+                        setWords.remove(modifiedStr)
+                    }
+                }
+                wordArray[i] = temp
+            }
+        }
+        return 0
+     }
+}

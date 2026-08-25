c5d9024a70cf53be7a434d3e858ed0cab612786e
Hirendra Sharma <hks.it11@gmail.com>
2026-08-25 11:44:33 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-0

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-0.swift
@@ -0,0 +1,11 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var profit = 0
+        for i in 1..<prices.count {
+            if prices[i] > prices[i-1] {
+                profit += prices[i] - prices[i-1]
+            }
+        }
+        return profit
+    }
+}

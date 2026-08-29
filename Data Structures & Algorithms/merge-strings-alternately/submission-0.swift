cb57263bcdbd3ffc200b4c698951d7e6f9969d54
Hirendra Sharma <hks.it11@gmail.com>
2026-08-29 11:12:45 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.swift
@@ -0,0 +1,27 @@
+class Solution {
+    func mergeAlternately(_ word1: String, _ word2: String) -> String {
+        let word1Chars = Array(word1)
+        let word2Chars = Array(word2)
+        var i = 0
+        var j = 0
+        var result = ""
+
+        while i < word1Chars.count && j < word2Chars.count {
+            result.append(word1Chars[i])
+            result.append(word2Chars[j])
+            i += 1
+            j += 1
+        }
+
+        while i < word1Chars.count {
+            result.append(word1Chars[i])
+            i += 1
+        } 
+
+        while j < word2Chars.count {
+            result.append(word2Chars[j])
+            j += 1
+        } 
+        return result
+    }
+}
